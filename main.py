8874d965210e2bc617665f5137720335e822d304
amshal1 <panshal911@gmail.com>
2024-01-04 17:59:28 +0530

    added some more comments

--- main.py
+++ main.py
@@ -13,16 +13,17 @@ for ext in supported_ext:
 
 
 for file in files:
-    # os.path.splitext return a tuple 1element is name of file last element is the extension name
+    # os.path.splitext return a tuple 1'st element is the name of file last element is the extension name
+    # unpacking the tuple _ gets the name of file file_extension get's the file extension
     _, file_extension = os.path.splitext(file)
-    # slicing the string because the first character is the dot and we so not want the dot
+    # slicing the string because the first character is the dot and we do not want the dot
     file_extension = file_extension[1:].lower()
     # checking if the file_extension is in supported_ext
     if file_extension in supported_ext:
         # current path of the file we are looping
         current_path = os.path.join(downloads_path, file)
-        # new path of the file
+        # new path for the file
         newpath = os.path.join(downloads_path, file_extension, file)
-        # if file does not exists in the new path and exists in the current path only then replace other wise do not do replacing
+        # if file does not exists in the new path and exists in the current path, only then replace other wise do not do replacing
         if not os.path.exists(newpath) and os.path.exists(current_path):
             os.replace(current_path, newpath)
